11da965a845c9c61d8caef0ea1f119497129d575
Lee haemun <lhmun1129@daum.net>
2022-07-18 18:04:16 +0900

    폰트어썸 사용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import Header from './Header';
+import Footer from './Footer';
+import './scss/style.scss';
+
 function App() {
 	return (
-		<ul className='list'>
-			<li>1</li>
-			<li>2</li>
-			<li>3</li>
-			<li>4</li>
-			<li>5</li>
-		</ul>
+		<>
+			<Header />
+			<Footer />
+		</>
 	);
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<footer>
+			<p>2022 &copy; All Right Reserved. </p>
+		</footer>
+	);
+}
+
+export default Footer;
