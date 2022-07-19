43dde56233c9fdfeef41a492651b6129891597f8
Lee haemun <lhmun1129@daum.net>
2022-07-19 17:51:36 +0900

    useState 사용, 비구조화할당

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import './scss/style.scss';
-import Card from './Card';
+import { useState } from 'react';
 
 function App() {
-	const path = process.env.PUBLIC_URL;
-
-	const imgs = [
-		'Blizzards',
-		'Calm',
-		'Dusty_Road',
-		'Escape',
-		'Payday',
-		'Retreat',
-		'Seasonal',
-		'Vespers',
-	];
+	console.log('app');
+	const [Data, setData] = useState('변경전');
+	const arr = ['red', 'green', 'blue'];
+	const [Colors, setColors] = useState(arr);
 
 	return (
 		<div className='App'>
-			<section>
-				{imgs.map((img, idx) => {
-					return <Card key={idx} img={img} path={path} />;
+			<button
+				onClick={() => {
+					setData('변경후');
+				}}>
+				{Data}
+			</button>
+
+			<button
+				onClick={() => {
+					const newColors = [...Colors];
+					newColors[0] = 'hotpink';
+					console.log(newColors);
+					setColors(newColors);
+				}}>
+				배열 state 변경
+			</button>
+			<ul>
+				{Colors.map((color, idx) => {
+					return <li key={idx}>{color}</li>;
 				})}
-			</section>
+			</ul>
 		</div>
 	);
 }
