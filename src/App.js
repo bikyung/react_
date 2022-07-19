646c07e02b9d2181382621e0b331f0be5da96196
Lee haemun <lhmun1129@daum.net>
2022-07-19 14:52:47 +0900

    반복문을 사용 스타일 변경

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-import { Header, Footer, Popup } from './Layout';
 import './scss/style.scss';
 
 function App() {
-	const handleClick = () => {
-		alert('click2');
-	};
+	const colors = ['aqua', 'orange', 'blue', 'red', 'green'];
 
-	const handleArg = (txt) => {
-		alert(txt);
-	};
 	return (
-		<div className='app'>
-			<h1
-				onMouseEnter={() => {
-					console.log('mouseEnter');
-				}}
-				onMouseLeave={() => console.log('mouseleave')}>
-				Title
-			</h1>
-			<button onClick={() => console.log('click')}>버튼</button>
-			<button onClick={handleClick}>버튼2</button>
-			<button
-				onClick={() => {
-					handleArg('hello');
-				}}>
-				버튼3
-			</button>
+		<div className='App'>
+			<ul>
+				{colors.map((color, idx) => {
+					const style = { color: color };
+					return (
+						<li key={idx} style={style}>
+							{color}
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 }
