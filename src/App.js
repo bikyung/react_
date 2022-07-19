b8a01559dd671c1b3e8b7f3fcb1d8b7e656255bf
Lee haemun <lhmun1129@daum.net>
2022-07-19 13:32:28 +0900

    이벤트 사용방법

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import Header from './Header';
-import Footer from './Footer';
+import { Header, Footer, Popup } from './Layout';
 import './scss/style.scss';
 
 function App() {
+	const handleClick = () => {
+		alert('click2');
+	};
+
+	const handleArg = (txt) => {
+		alert(txt);
+	};
 	return (
-		<>
-			<Header />
-			<Footer />
-		</>
+		<div className='app'>
+			<h1
+				onMouseEnter={() => {
+					console.log('mouseEnter');
+				}}
+				onMouseLeave={() => console.log('mouseleave')}>
+				Title
+			</h1>
+			<button onClick={() => console.log('click')}>버튼</button>
+			<button onClick={handleClick}>버튼2</button>
+			<button
+				onClick={() => {
+					handleArg('hello');
+				}}>
+				버튼3
+			</button>
+		</div>
 	);
 }
 
---
+++ src/Layout.js
@@ -0,0 +1,25 @@
+function Header() {
+	return (
+		<header>
+			<h1>logo</h1>
+		</header>
+	);
+}
+
+function Footer() {
+	return (
+		<footer>
+			<h1>footer</h1>
+		</footer>
+	);
+}
+
+function Popup() {
+	return (
+		<aside>
+			<h1>Popup</h1>
+		</aside>
+	);
+}
+
+export { Header, Footer, Popup };
